edd5ae3e0a1cab3a5b9c603ed32e1a8eddf9af8f
Ashritha <janagamashrithareddy@gmail.com>
2026-02-11 22:30:12 +0530

    first commit

---
+++ reverse_string.py
@@ -0,0 +1,20 @@
+s="python"
+new=""
+
+for char in s:
+    new=char+new 
+
+print(new)
+
+
+## 
+s = "python"
+reversed_s = "".join(reversed(s))
+print(reversed_s)
+
+
+## 
+s="python"
+print(s[::-1])
+
+
